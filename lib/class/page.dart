7a2abe1898d26df033ec40fd78e9472cf57091f4
AndreFouquart <122102727+AndreFouquart@users.noreply.github.com>
2023-11-20 19:29:36 +0100

    Image mis au centre (oubli de ma part)

--- lib/class/page.dart
+++ lib/class/page.dart
@@ -37,7 +37,9 @@ class PremierePage extends StatelessWidget {
                 'et de vous fier aux instructions (à diluer dans de l’eau dans laquelle '
                 'il faut faire tremper le matériel quelques minutes).',
           ),
-          Image(image: AssetImage("assets/images/page0/step00.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page0/step00.png")),
+          ),
         ],
       ),
     );
@@ -66,11 +68,15 @@ class DeuxiemePage extends StatelessWidget {
                   'Le malt blond peut s\'utiliser seul. Les malts plus foncés ne s’utilisent qu’en petite dose pour moduler le goût de la bière. '
                   'Si vous avez acheté du malt en grain entier, le concassez avec un moulin à céréales, un mixeur ou encore un rouleau à pâtisserie. '
           ),
-          Image(image: AssetImage("assets/images/page1/step10.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page1/step10.png")),
+          ),
           Text(
             'Définissez la quantité d’eau de brassage nécessaire, puis versez-la dans une grande casserole. Porter cette eau à une température de 50 °C en vérifiant avec le thermomètre, puis ajoutez le malt concassé.',
           ),
-          Image(image: AssetImage("assets/images/page1/step11.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page1/step11.png")),
+          )
         ],
       ),
     );
@@ -105,7 +111,10 @@ class TroisiemePage extends StatelessWidget {
               'Augmenter la température à 68-70°C et maintenir cette température environ 30 min. '
               'Augmenter la température à 78°C et maintenir cette température environ 30 min.',
           ),
-          Image(image: AssetImage("assets/images/page2/step21.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page2/step21.png")),
+          )
+
         ],
       ),
     );
@@ -132,11 +141,15 @@ class QuatriemePage extends StatelessWidget {
           Text(
               'Au dessus d’une seconde casserole, verser la maîshe dans une grande passoire (tapisser si besoin d’un linge propre) pour obtenir la drêche (résidu solide du malt). '
           ),
-          Image(image: AssetImage("assets/images/page3/step30.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page3/step30.png")),
+          ),
           Text('Pour récupérer les sucres restants dans la drêche égouttée, verser au-dessus de la drêche l’eau de rinçage préalablement montée à 80°C. '
               'L’eau de brassage associé à l’eau de rinçage s’appelle le moût. Les résidus de malt sont compostables.',
           ),
-          Image(image: AssetImage("assets/images/page3/step31.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page3/step31.png")),
+          )
         ],
       ),
     );
@@ -165,7 +178,9 @@ class CinquiemePage extends StatelessWidget {
                   'Faites bouillir le moût. Dès que l\'ébullition commence, écumez et ajoutez le houblon amer. '
                   'Laissez bouillir ce mélange sans couvrir pendant 1h30 pour retirer de l’amertume. '
           ),
-          Image(image: AssetImage("assets/images/page4/step40.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page4/step40.png")),
+          ),
           Text('10-15 minutes avant la fin de cette ébullition, ajoutez le houblon aromatique. Retirez le houblon en le filtrant. '
               'Évitez cette étape en plaçant le houblon dans un filtre à thé en papier. '
               'Le choix du houblon doit être fait en fonction de son taux IBU qui est l’unité d’amertume.',
@@ -196,21 +211,29 @@ class SixiemePage extends StatelessWidget {
           Text(
               'Le moût doit être refroidi à une température de 20-25°C pour y incorporer les levures puis transvasé dans la dame-jeanne. '
           ),
-          Image(image: AssetImage("assets/images/page5/step50.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page5/step50.png")),
+          ),
           Text(
               'Il existe différentes levures. Nous vous recommandons d’utiliser des levures sèches qui sont mieux adaptées pour des petits brassins. '
                   'Se référer à l’emballage pour déterminer au mieux la quantité appropriée. Il est recommandé d’utiliser pour débuter de la levure S33 qui est polyvalente. '
           ),
-          Image(image: AssetImage("assets/images/page5/step51.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page5/step51.png")),
+          ),
           Text('Incorporer la levure et remuer la bonbonne pour mélanger. Dans une pièce à 20°C, fermer la bonbonne et placer le barboter (qui permet d’évacuer l’oxygène). '
           ),
-          Image(image: AssetImage("assets/images/page5/step52.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page5/step52.png")),
+          ),
           Text(
               'Durant environ une semaine, les levures vont travailler et générer du CO2 dans la bonbonne, qui va s’évacuer par le barboteur. '
                   'Remuer de temps en temps la bonbonne pour faire évacuer le CO2. Lorsqu’il n’y a plus d’activité dans le barboteur, mettre la bouteille de fermentation dans une pièce plus fraîche (autour de 10-15°C, comme une cave par exemple). '
                   'La bière peut ainsi maturer pendant 1 à 3 semaines, cette période de fermentation secondaire se nomme la garde. '
           ),
-          Image(image: AssetImage("assets/images/page5/step53.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page5/step53.png")),
+          ),
           Text(
             'Ajouter selon recette des épices, fruits, ...',
           ),
@@ -240,7 +263,9 @@ class SeptiemePage extends StatelessWidget {
           Text(
             'Pour relancer la fermentation qui se fera en bouteille et qui permet d’obtenir de la mousse, incorporer 4 à 6 g de sucre par litre de bière.',
           ),
-          Image(image: AssetImage("assets/images/page6/step60.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page6/step60.png")),
+          ),
         ],
       ),
     );
@@ -267,13 +292,17 @@ class HuitiemePage extends StatelessWidget {
           Text(
               'Mettre en bouteille (stérilisées et séchées) en utilisant un tuyau d\'extraction avec embout anti-lie. '
           ),
-          Image(image: AssetImage("assets/images/page7/step70.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page7/step70.png")),
+          ),
           Text(
               'Le but est de récupérer le liquide clair et d’éliminer les précipités et dépôts. Placer l’embout anti-lie sur le fond de la bonbonne et l’autre bout au fond de la bouteille. La bonbonne doit être placée très en hauteur par rapport à la bouteille. '
                   'Utiliser des bouteilles en verre teinté propres et sèches, qui ferment avec un bouchon à vis ou à clapet. Les bouteilles doivent être conservées verticalement. '
                   'Pendant une semaine conserver ces bouteilles à 20°C. Puis pendant 4 à 8 semaines, les garder au frais, entre 6 et 10°C. '
           ),
-          Image(image: AssetImage("assets/images/page7/step71.png")),
+          Center(
+            child: Image(image: AssetImage("assets/images/page7/step71.png")),
+          ),
           Text(
             'La bière se conserve 1 an, idéalement entre 10 et 15°C à l’abri de la lumière ou simplement au réfrigérateur.',
           ),
@@ -300,7 +329,9 @@ class DernierePage extends StatelessWidget {
               )
           ),
           SizedBox(height: 8),
-          Image(image: AssetImage("assets/images/page8/step80.png"))
+          Center(
+            child: Image(image: AssetImage("assets/images/page8/step80.png"))
+          )
         ],
       ),
     );
